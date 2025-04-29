6b1d92549fac7a29fa06422fbda6121647c34231
Mioara Cenusa <mioaracenusa82@gmail.com>
2025-04-29 01:40:24 +0100

    commented out unused variables

--- src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { SignIn } from "@clerk/nextjs";
-import { usePathname } from "next/navigation";
+//import { usePathname } from "next/navigation";
 import { authStyles } from "../../authStyles";
 import { useHomeRedirect } from "../../hooks";
 
 const LoginPage = () => {
-	const pathname = usePathname();
+	//const pathname = usePathname();
 	//const incorrectPassword = pathname.includes("factor-one");
 
 	useHomeRedirect();
