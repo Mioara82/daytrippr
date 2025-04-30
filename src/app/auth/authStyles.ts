37b5dd9ca444cc1595bafd77f357dcdb311b8d1f
Mioara Cenusa <mioaracenusa82@gmail.com>
2025-04-30 17:57:47 +0100

    delete auth folder

--- src/app/auth/authStyles.ts
+++
@@ -1,7 +0,0 @@
-import type {Elements} from "@clerk/types";
-
-export const authStyles:Elements = {
-    colorPrimary: "cyan-500",
-    colorTextPrimary: "gray-900",
-    colorTextSecondary: "gray-500",
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import LoginPage from "./auth/sign-in/[[...rest]]/page";
-import UserRegisterPage from "./auth/sign-up/[[...rest]]/page";
-
 export default function Home() {
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-primary ">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-				{/* <LoginPage /> */}
 				<h1>Hello</h1>
 			</main>
 		</div>
