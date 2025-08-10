edc4f2a5f13c1fd5d2d7d2ec9dab11c5756902bf
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2025-08-10 20:58:56 +0100

    feat: setup supabase users table and RLS policies. synced with clerk for user authentication and authorization

---
+++ src/lib/hooks/useSupabaseClient.ts
@@ -0,0 +1,36 @@
+import { useAuth } from "@clerk/nextjs";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { useMemo } from "react";
+import type { Database } from "../../../database.types";
+
+export default function useSupabaseClient(): SupabaseClient<Database> {
+  const { getToken } = useAuth()
+
+  const supabase = useMemo(() => {
+    return createClient<Database>(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        global: {
+          fetch: async (url, options = {}) => {
+            const clerkToken = await getToken({
+              template: "supabase",
+            })
+
+            const headers = new Headers(options?.headers)
+            if (clerkToken) {
+              headers.set("Authorization", `Bearer ${clerkToken}`)
+            }
+
+            return fetch(url, {
+              ...options,
+              headers,
+            })
+          },
+        },
+      }
+    )
+  }, [getToken]);
+
+  return supabase
+};
