330703c32ce8ede9bcac418c1a0b193f9e6d7f58
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2025-08-12 21:11:05 +0100

    feat: delete unused pages

--- src/app/planned-trips/page.tsx
+++
@@ -1,8 +0,0 @@
-const PlannedTripsPage = () => {
-    return (
-        <div>
-        <h1>Planned Trips</h1>
-        <p>Here you can view and manage your planned trips.</p>
-        </div>
-    );
-}
--- src/app/web/auth/[[...sign-in]].tsx
+++
@@ -1,2 +0,0 @@
-import { useUser } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
